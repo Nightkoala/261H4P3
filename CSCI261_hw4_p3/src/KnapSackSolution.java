b05b3a5dec73c18ce77762dd6f08827b45ab79bb
Derek Brown <djb3718@rit.edu>
2014-10-14 14:24:00 -0400

    Added to Eclipse and created java file

---
+++ CSCI261_hw4_p3/src/KnapSackSolution.java
@@ -0,0 +1,15 @@
+/**
+ * KnapSackSolution.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ *
+ * Purpose	Reconstruct the answer to the knapsack problem and return the
+ * 			results.
+ */
+
+public class KnapSackSolution {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end KnapSackSolution
